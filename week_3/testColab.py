8a749819b23d4e225e6d23abb6ac329fffa01017
Reyes <dareyes14@cps.k12.il.us>
2024-09-17 12:47:12 -0500

    new_assignment_weeK_4

---
+++ week_3/testColab.py
@@ -0,0 +1,6 @@
+#this is just for testing 
+#for collab purposes 
+#create an input for the user to input their age
+#create an input for the user to input their name 
+#create an input for the user to input their favorite song 
+
